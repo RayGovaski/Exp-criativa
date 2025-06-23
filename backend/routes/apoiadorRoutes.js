9fe159bb4a4dcac14e2dcdc414311bb9a5b08e59
Thais Amaral <134018464+thaismaral@users.noreply.github.com>
2025-06-23 12:58:22 -0300

    Update apoiadorRoutes.js

--- backend/routes/apoiadorRoutes.js
+++ backend/routes/apoiadorRoutes.js
@@ -17,24 +17,19 @@ import {
 
 
 import { uploadToMemory } from '../config/multer.js';
-
 import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
-
-// --- ROTAS DO APOIADOR ---
 
 router.post('/', uploadToMemory.single('foto'), createApoiador);
 router.put('/update-foto', verifyToken, uploadToMemory.single('foto'), updateApoiadorFoto);
 router.get('/perfil', verifyToken, getProfile);
 router.get('/foto/:id', getApoiadorFoto);
-
 router.put('/update-senha', verifyToken, updatePassword);
 router.put('/update-email', verifyToken, updateEmail);
 router.put('/update-telefone', verifyToken, updatePhone);
 router.get('/historico-doacoes', verifyToken, getApoiadorDonationsHistory);
 router.get('/relatorios', verifyToken, getApoiadorReports);
-
 router.delete('/cancelar-assinatura', verifyToken, cancelSubscription);
 router.post('/assinar-plano', verifyToken, subscribeToPlan);
 

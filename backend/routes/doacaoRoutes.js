45cabeda3bc6eb6077772ca481fec25d5e7c9fb8
Thais Amaral <134018464+thaismaral@users.noreply.github.com>
2025-06-23 12:59:53 -0300

    Update doacaoRoutes.js

--- backend/routes/doacaoRoutes.js
+++ backend/routes/doacaoRoutes.js
@@ -11,26 +11,15 @@ import {
 } from '../controllers/doacaoController.js';
 
 import { uploadToMemory } from '../config/multer.js';
-
 import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
 
-
-// Rota para a home page, que pega as 4 doações principais
 router.get('/', get4Doacoes);
-
-// Rota para a página "Doar", que lista todas as doações
 router.get('/todas', getAllDoacoesFull);
-
-// Rota para ver os detalhes de uma doação específica
 router.get('/:id', getDoacaoById);
-
-// Rota para servir a imagem de uma doação
 router.get('/imagem/:id', getDoacaoImage);
-
 router.post('/processar', verifyToken, processDonation);
-
 router.post('/criar', verifyToken, uploadToMemory.single('imagem'), createDoacaoCard);
 
 
